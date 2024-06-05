d8655ee230f16caa56042b20eb8d75395c36a6b9
Yan Holtz <yan.holtz.data@gmail.com>
2024-06-05 15:16:06 +0200

    export modal

--- src/components/ExportDialogButton.tsx
+++ src/components/ExportDialogButton.tsx
@@ -9,8 +9,19 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "./ui/button";
 import { Download } from "lucide-react";
+import { ColorPalette } from "@/data/color-palette-list";
+import { getColorListFromString } from "@/lib/utils";
 
-export const ExportDialogButton = () => {
+export const ExportDialogButton = ({
+  selectedColorObject,
+}: {
+  selectedColorObject: ColorPalette;
+}) => {
+  const snippetPythonCode = `
+from pypalettes import load_cmap
+cmap = load_cmap("${selectedColorObject.name}")
+`.trim();
+
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -18,19 +29,38 @@ export const ExportDialogButton = () => {
           <Download />
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className="sm:max-w-[800px] py-20">
         <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
+          <DialogTitle>Export this palette</DialogTitle>
           <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
+            Choose your favorite way to export those colors.
           </DialogDescription>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <p>HEllo</p>
+        <div className="py-4 overflow-scroll">
+          <DialogTitle>Array of string:</DialogTitle>
+          <div className="bg-gray-200 rounded-sm mt-2 p-4 text-xs">
+            <pre>
+              <code>
+                {getColorListFromString(selectedColorObject.palette).map(
+                  (col) => "'" + col + "', "
+                )}
+              </code>
+            </pre>
+          </div>
+
+          <br />
+
+          <DialogTitle>Python code</DialogTitle>
+          <DialogDescription className="mt-2">
+            Note that you'll need to install the <code>pypalettes</code> library
+            first.
+          </DialogDescription>
+          <div className="bg-gray-200 rounded-sm mt-2 p-4 text-xs leading-6">
+            <pre>
+              <code>{snippetPythonCode}</code>
+            </pre>
+          </div>
         </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   );
