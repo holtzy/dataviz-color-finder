01243f067568c9c9928e18a38810c3c4bcfd2cde
Yan Holtz <yan.holtz.data@gmail.com>
2024-06-05 11:44:20 +0200

    add select button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { ColorPaletteSelectButton } from "@/components/ColorPaletteSelectButton";
 import { Barplot } from "@/dataviz/barplot/Barplot";
 import { barplotData } from "@/dataviz/barplot/data";
 import { ChoroplethMap } from "@/dataviz/choropleth/ChoroplethMap";
@@ -9,13 +12,20 @@ import { pieData } from "@/dataviz/piechart/data";
 import { Treemap } from "@/dataviz/treemap/Treemap";
 import { treemapData } from "@/dataviz/treemap/data";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [colorPalette, setColorPalette] = useState("");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          A project by the{" "}<a href="https://www.python-graph-gallery.com"> Python Graph Gallery</a>
+          A project by the{" "}
+          <a href="https://www.python-graph-gallery.com">
+            {" "}
+            Python Graph Gallery
+          </a>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
           <a
@@ -37,6 +47,8 @@ export default function Home() {
         </div>
       </div>
 
+      <ColorPaletteSelectButton />
+
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Barplot data={barplotData} width={300} height={300} />
         <Heatmap data={heatmapData} width={300} height={300} />
@@ -44,7 +56,13 @@ export default function Home() {
       </div>
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Treemap data={treemapData} width={300} height={300} />
-        <ChoroplethMap geoData={geoData} numData={numData} width={800} height={800} />,
+        <ChoroplethMap
+          geoData={geoData}
+          numData={numData}
+          width={800}
+          height={800}
+        />
+        ,
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
