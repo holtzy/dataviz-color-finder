6a19929a16dc58f27af3eebd59003f1e6dba1dfa
Yan Holtz <yan.holtz.data@gmail.com>
2024-06-05 14:59:45 +0200

    initialize dialog button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ColorPaletteSelectButton } from "@/components/ColorPaletteSelectButton";
+import { ExportDialogButton } from "@/components/ExportDialogButton";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { colorPaletteList } from "@/data/color-palette-list";
@@ -15,7 +16,7 @@ import { pieData } from "@/dataviz/piechart/data";
 import { Treemap } from "@/dataviz/treemap/Treemap";
 import { treemapData } from "@/dataviz/treemap/data";
 import { getColorListFromString } from "@/lib/utils";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { ArrowLeft, ArrowRight, Download, Filter } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -67,6 +68,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex gap-6 items-top">
+        <Button variant={"outline"}>
+          <Filter />
+        </Button>
+
         <div className="flex flex-col">
           <ColorPaletteSelectButton
             paletteList={colorPaletteList}
@@ -91,6 +96,8 @@ export default function Home() {
             <ArrowRight size={15} />
           </Button>
         </div>
+
+        <ExportDialogButton />
       </div>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
---
+++ src/components/ExportDialogButton.tsx
@@ -0,0 +1,37 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
+import { Download } from "lucide-react";
+
+export const ExportDialogButton = () => {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">
+          <Download />
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Edit profile</DialogTitle>
+          <DialogDescription>
+            Make changes to your profile here. Click save when you're done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <p>HEllo</p>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
