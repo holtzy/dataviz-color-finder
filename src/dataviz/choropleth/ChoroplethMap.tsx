2096bf969c3bd267ebaf1437c37db78c280b4d91
Yan Holtz <yan.holtz.data@gmail.com>
2024-06-06 15:46:08 +0200

    better choopleth

--- src/dataviz/choropleth/ChoroplethMap.tsx
+++ src/dataviz/choropleth/ChoroplethMap.tsx
@@ -1,6 +1,8 @@
 import * as d3 from "d3";
 import { FeatureCollection } from "geojson";
 
+const CENTER: [number, number] = [30, 65]; // The geographic center of your map
+
 type ChoroplethMapProps = {
   width: number;
   height: number;
@@ -16,16 +18,40 @@ export const ChoroplethMap = ({
   numData,
   colorList,
 }: ChoroplethMapProps) => {
-  var colorScale = d3
+  const colorScale = d3
     .scaleThreshold<number, string>()
     .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
     .range(colorList);
 
   const projection = d3
     .geoMercator()
-    .scale(width / 2 / Math.PI - 20)
-    .center([10, 55]);
+    .center(CENTER)
+    .scale(0.5) // Start with a scale of 1
+    .translate([0, 0]); // Start with no translation
 
+  // Create a path generator with the initial projection
+  const path = d3.geoPath().projection(projection);
+
+  // Compute the bounds of a feature or the entire map (if you have a specific feature to fit, use that instead)
+  const bounds = path.bounds(geoData); // geoData is your GeoJSON data
+
+  // Calculate the scale and translation
+  const scale =
+    0.95 /
+    Math.max(
+      (bounds[1][0] - bounds[0][0]) / width,
+      (bounds[1][1] - bounds[0][1]) / height
+    );
+
+  const translate: [number, number] = [
+    (width - scale * (bounds[1][0] + bounds[0][0])) / 2,
+    (height - scale * (bounds[1][1] + bounds[0][1])) / 2,
+  ];
+
+  // Update the projection with the new scale and translation
+  projection.scale(scale).translate(translate);
+
+  // Recreate the path generator with the updated projection
   const geoPathGenerator = d3.geoPath().projection(projection);
 
   const allSvgPaths = geoData.features
