045049c175f0ef88fd980107781603425e975138
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2024-06-14 10:17:01 +0200

    update code snippet + other examples

--- src/lib/get-chart-code-R.ts
+++ src/lib/get-chart-code-R.ts
@@ -1,16 +1,21 @@
 export const getBarplotCode = (palette: string) => {
 
     const code = `
-# load necessary libraries
+# Load the required libraries
 library(ggplot2)
 library(paletteer)
 
-# load the data
-data <- read.csv("https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-barplot.csv")
+# Create a sample dataset
+data <- read_csv(
+   "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-barplot.csv",
+   show_col_types = FALSE
+)
+
+# Reorder the factor levels to display the bars in the correct order
 data$name <- factor(data$name, levels = rev(data$name))
 data <- mutate(data, name = fct_reorder(name, value))
 
-# create the bar plot
+# Create the plot
 ggplot(data, aes(x = value, y = name, fill = name)) +
    geom_col() +
    scale_fill_paletteer_d("${palette}") +
@@ -27,21 +32,21 @@ ggplot(data, aes(x = value, y = name, fill = name)) +
 export const getHeatmapCode = (palette: string) => {
 
     const code = `
-# load required libraries
+# Load required libraries
 library(ggplot2)
 library(paletteer)
 library(reshape2)
 
-# create a sample matrix to simulate data
+# Create a sample matrix to simulate data
 set.seed(123)
 data_matrix <- matrix(runif(625, min = 0, max = 1), nrow = 25, ncol = 25)
 rownames(data_matrix) <- rev(LETTERS[1:25])
 colnames(data_matrix) <- LETTERS[1:25]
 
-# melt the matrix to a long format
+# Melt the matrix to a long format
 data_long <- melt(data_matrix)
 
-# create the heatmap
+# Plot using ggplot2
 ggplot(data_long, aes(x = Var2, y = Var1, fill = value)) +
    geom_tile() +
    scale_fill_paletteer_c("${palette}") +
@@ -60,19 +65,20 @@ ggplot(data_long, aes(x = Var2, y = Var1, fill = value)) +
 export const getPieChartCode = (palette: string) => {
 
     const code = `
-# load necessary libraries
+# Load the required libraries
 library(ggplot2)
 library(paletteer)
 
-# sample data
-data <- read.csv2("https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-piechart.csv")
+# Load the data
+data <- read.csv("https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-piechart.csv")
 
-# create the pie chart
+# Create the pie chart
 ggplot(data, aes(x = "", y = value, fill = name)) +
    geom_bar(stat = "identity", width = 1) +
+   geom_text(aes(label = paste0(name, ": ", value)), position = position_stack(vjust = 0.5)) +
    coord_polar(theta = "y") +
    theme_void() +
-   scale_fill_paletteer_d(${palette}) +
+   scale_fill_paletteer_d("${palette}") +
    theme(legend.position = "none") +
    labs(fill = "Category")`.trim();
 
@@ -82,14 +88,14 @@ ggplot(data, aes(x = "", y = value, fill = name)) +
 export const getBubblePlotCode = (palette: string) => {
 
     const code = `
-# load necessary libraries
+# Load necessary libraries
 library(ggplot2)
 library(paletteer)
 
-# load the data
-df <- read.csv2("https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-scatterplot.csv")
+# Load the data
+df <- read.csv("https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-scatterplot.csv")
 
-# create the bubble plot
+# Create the ggplot
 ggplot(df, aes(x = gdpPercap, y = lifeExp, size = pop, color = continent)) +
    geom_point(size=10) +
    scale_color_paletteer_d("${palette}") +
@@ -102,8 +108,24 @@ ggplot(df, aes(x = gdpPercap, y = lifeExp, size = pop, color = continent)) +
 export const getTreemapCode = (palette: string) => {
 
     const code = `
-Not available yet.
-WIP`.trim();
+# Load necessary libraries
+library(treemapify)
+library(ggplot2)
+library(paletteer)
+
+# Load the data
+df <- read_csv(
+   "https://raw.githubusercontent.com/holtzy/The-Python-Graph-Gallery/master/static/data/simple-treemap.csv",
+   show_col_types = FALSE
+)
+
+# Create the treemap
+ggplot(df, aes(area = value, fill = parent, label = name)) +
+   geom_treemap() +
+   geom_treemap_text(colour = "white",
+                     place = "centre") +
+   scale_fill_paletteer_d("${palette}") +
+   theme(legend.position = "none")`.trim();
 
     return code
 }
@@ -111,27 +133,30 @@ WIP`.trim();
 export const getChoroplethCode = (palette: string) => {
 
     const code = `
-# load necessary libraries
+# Load necessary libraries
 library(sf)
 library(ggplot2)
+library(paletteer)
 
 # Geospatial data available at the geojson format
 tmp_geojson <- tempfile(fileext = ".geojson")
 download.file(
-   "https://raw.githubusercontent.com/holtzy/R-graph-gallery/DATA/world.geojson",
+   "https://raw.githubusercontent.com/JosephBARBIERDARNAL/R-graph-gallery/master/DATA/world.geojson",
    tmp_geojson
 )
 my_sf <- read_sf(tmp_geojson)
-my_sf <- my_sf[my_sf$name != "Antarctica", ] # remove antarctica
-my_sf$random_value <- runif(nrow(my_sf)) # add numeric values
 
-# create the choropleth
+# Remove antarctica from the map
+my_sf <- my_sf[my_sf$name != "Antarctica", ]
+
+# Add random numeric values to the map
+my_sf$random_value <- runif(nrow(my_sf))
+
 ggplot(my_sf) +
    geom_sf(aes(fill = random_value), color = "black", linewidth = 0.3) +
    theme_void() +
    theme(legend.position = "none") +
-   scale_fill_paletteer_c("ggthemes::Classic Orange")
-`.trim();
+   scale_fill_paletteer_c("${palette}")`.trim();
 
     return code
 }
@@ -139,8 +164,22 @@ ggplot(my_sf) +
 export const getStreamchartCode = (palette: string) => {
 
     const code = `
-Not available yet.
-WIP`.trim();
+# Load necessary libraries
+library(ggstream)
+library(ggplot2)
+library(paletteer)
+
+# Load the data
+df <- read.csv("https://raw.githubusercontent.com/holtzy/R-graph-gallery/master/DATA/stream.csv")
+
+# Create the streamgraph
+ggplot(df, aes(x = year, y = value, fill = name)) +
+   geom_stream(color = "black") +
+   scale_fill_paletteer_d("${palette}") +
+   theme_minimal() +
+   theme(legend.position = "none") +
+   labs(title = "Streamgraph of the datastream dataset", x = "Year", y = "Value") +
+   theme(plot.title = element_text(hjust = 0.5))`.trim();
 
     return code
 }
